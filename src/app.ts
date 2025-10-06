0d3d9ab5dda4c028c023866e8ae9f0bfe9733db2
HaNguyen051 <vanha03092005@gmail.com>
2025-10-06 22:33:00 +0700

    ejs

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,15 @@ import 'dotenv/config'
 const app = express();
 const port = process.env.PORT || 8080; 
 
+//config view engine 
+ app.set('view engine', 'ejs') ;
+app.set('views', __dirname + '/views');
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World with nodemon</h1>");
+  res.render("home")
  
 });
+//src\views
 app.get("/abc", (req, res) => {
    res.send('<h1 style = "color : red">nodemon</h1>');
 });
