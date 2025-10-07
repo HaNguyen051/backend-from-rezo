a973356a8da3f5470c3b18431afe972ece55f561
HaNguyen051 <vanha03092005@gmail.com>
2025-10-07 08:04:11 +0700

    html form

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,12 +1,14 @@
 
 import express, {Express} from "express" ;
-import { getCreateUserPage, getHomePage } from "../controllers/user.controller";
+import { getCreateUserPage, getHomePage, postCreateUserPage } from "../controllers/user.controller";
 const router = express.Router() 
 
 const webRoutes = (app :Express) => {
     router.get("/", getHomePage);
     //src\views
     router.get("/create-user", getCreateUserPage) ; 
+    //giong nhau url van ko van de j 
+    router.post("/handle-create-user", postCreateUserPage) ; 
 
     app.use('/', router); 
 }
