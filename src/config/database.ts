59feb87cfa8d7d5f5e9526f0af371097d11da5d3
HaNguyen051 <vanha03092005@gmail.com>
2025-10-07 11:45:37 +0700

    fake data mysql

---
+++ src/config/database.ts
@@ -0,0 +1,25 @@
+// Get the client
+import mysql from 'mysql2/promise';
+
+// Create the connection to database
+const getConnection = async () =>{
+    const connection = await mysql.createConnection({
+    port: 3306 , 
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejspro',
+    password :'123456'
+    });
+try {
+  const [results, fields] = await connection.query(
+    'SELECT * FROM `users`'
+  );
+
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+  console.log(err);
+}
+
+}
+export default getConnection ; 
